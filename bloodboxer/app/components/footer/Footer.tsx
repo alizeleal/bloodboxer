afbaf2f043388da5678561cf458f24ab48811497
Alize Leal Pereira <alize.de.fatima@gmail.com>
2024-10-11 03:34:36 -0300

    creates  components and template

---
+++ bloodboxer/app/components/footer/Footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => (
+    <div className="Footer-text">
+        <p>Centro de Treinamento Blood Boxer</p>
+      <address>Rua Olímpia Esteves, 1270 - Padre Miguel
+        Rio de Janeiro - RJ
+      </address>
+      <p>
+        Desenvolvida por Alize L. Pereira <time dateTime="2024">@2024</time>
+      </p>
+     
+    </div>
+  );
+  
+  export default Footer;
---
+++ bloodboxer/app/components/header/Header.tsx
@@ -0,0 +1,22 @@
+
+
+const Header = () => (
+
+
+    <ul className="NavigationBar">
+      <li>
+        <a href="#boxe">Boxe</a>
+      </li>
+      <li>
+        <a href="#muaythai">Muay Thai</a>
+      </li>
+      <li>
+        <a href="#jiujitsu">Jiu Jitsu</a>
+      </li>
+      <li>
+        <a href="#contato">Contato</a>
+      </li>
+    </ul>
+);
+
+export default Header;
